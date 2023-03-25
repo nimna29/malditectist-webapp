83390310e51d500ab3a2013783567ae7899b2574
Nimna Niwarthana <77341013+Nimna29@users.noreply.github.com>
2023-03-25 21:11:15 +0530

    Add .env file

--- malditectist/firebase.py
+++ malditectist/firebase.py
@@ -1,11 +1,25 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 
+# Get Firebase service account credentials from environment variables
+cred = credentials.Certificate({
+    'type': 'service_account',
+    'project_id': os.environ.get('FIREBASE_PROJECT_ID'),
+    'private_key_id': os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
+    'private_key': os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    'client_email': os.environ.get('FIREBASE_CLIENT_EMAIL'),
+    'client_id': os.environ.get('FIREBASE_CLIENT_ID'),
+    'auth_uri': os.environ.get('FIREBASE_AUTH_URI'),
+    'token_uri': os.environ.get('FIREBASE_TOKEN_URI'),
+    'auth_provider_x509_cert_url': os.environ.get('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+    'client_x509_cert_url': os.environ.get('FIREBASE_CLIENT_CERT_URL')
+})
+
 # Initialize the Firebase app with the service account credentials
-cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'malditectist-3ba37.appspot.com'
+    'storageBucket': os.environ.get('FIREBASE_STORAGE_BUCKET')
 })
 
 # Get a reference to the default bucket
